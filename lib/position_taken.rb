0626d7a458327e832f6b233c51f31047d156f093
Matthew Hunger <matt.hunger@gmail.com>
2017-09-29 11:42:09 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "X" || "O"
+    puts board[index]
+    status = false
+  else
+    puts board[index]
+    puts "test"
+    status = true 
+
+  end
+return status 
+end
+
+
+
+elsif board[index] == "X" ||  "O"
+  return true
