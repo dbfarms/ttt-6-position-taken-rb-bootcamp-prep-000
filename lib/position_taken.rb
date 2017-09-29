84428417c5059d7116fb3c9c8fa5c8f5c90082a2
Matthew Hunger <matt.hunger@gmail.com>
2017-09-29 11:44:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,13 +6,30 @@ def position_taken?(board, index)
   else
     puts board[index]
     puts "test"
-    status = true 
+    status = true
 
   end
-return status 
+return status
 end
 
 
 
 elsif board[index] == "X" ||  "O"
   return true
+
+
+  def position_taken?(board, index)
+    if board[index] == "X" 
+      puts board[index]
+      puts "i got here"
+      status = true
+    elsif board[index] == "O"
+      puts board[index]
+      puts "test"
+      status = true
+    elsif board[index] != "X" || "O"
+      return false
+    end
+  return status
+  end
+  
